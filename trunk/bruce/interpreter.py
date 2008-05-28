6c67e3de6fd8fe3abddb0a36a4e95c2104a26a20
r1chardj0n3s <r1chardj0n3s@1a22b027-3248-0410-905a-5b7c5a519ae3>
2008-05-28 00:34:54 +0000

    closer to a working interpreter element

--- trunk/bruce/interpreter.py
+++ trunk/bruce/interpreter.py
@@ -38,24 +38,18 @@ interpreter_directive.content = True
 directives.register_directive('interpreter', interpreter_directive)
 
 class MyScrollableTextLayoutGroup(pyglet.text.layout.ScrollableTextLayoutGroup):
-    scissor_offset_x = 0
-    scissor_offset_y = 0
-    view_offset_x = 0
-    view_offset_y = 0
     def set_state(self):
         glPushAttrib(GL_ENABLE_BIT | GL_SCISSOR_BIT | GL_CURRENT_BIT)
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         # Disable scissor to check culling.
         glEnable(GL_SCISSOR_TEST)
-        ox = self.scissor_offset_x + self.view_offset_x
-        oy = self.scissor_offset_y + self.view_offset_y
-        print ox, oy, (ox + self._scissor_x - 1,
-                  oy + self._scissor_y - self._scissor_height,
-                  self._scissor_width + 1,
-                  self._scissor_height)
-        glScissor(ox + self._scissor_x - 1,
-                  oy + self._scissor_y - self._scissor_height,
+
+        # offset the scissor based on the parent layout's screen translation
+        parent_x = self.parent_group.translate_x
+        parent_y = self.parent_group.translate_y
+        glScissor(parent_x + self._scissor_x - 1,
+                  parent_y + self._scissor_y - self._scissor_height,
                   self._scissor_width + 1,
                   self._scissor_height)
         glTranslatef(self.translate_x, self.translate_y, 0)
@@ -63,19 +57,54 @@ class MyScrollableTextLayoutGroup(pyglet.text.layout.ScrollableTextLayoutGroup):
 
 class ScrolledIncrementalTextLayout(pyglet.text.layout.IncrementalTextLayout):
     def _init_groups(self, group):
-        # Scrollable layout never shares group becauase of translation.   
+        # override with our scrolledd scrollable ... er layout
         self.top_group = MyScrollableTextLayoutGroup(group)
         self.background_group = pyglet.graphics.OrderedGroup(0, self.top_group)
         self.foreground_group = pyglet.text.layout.TextLayoutForegroundGroup(1, self.top_group)
         self.foreground_decoration_group = \
             pyglet.text.layout.TextLayoutForegroundDecorationGroup(2, self.top_group)
 
+class Output:
+    def __init__(self, display, realstdout):
+        self.out = display
+        self.realstdout = realstdout
+        self.data = ''
+
+    def write(self, data):
+        self.out(data)
+
+# XXX put this in a thread
+class MyInterpreter(code.InteractiveInterpreter):
+    def __init__(self, locals, display):
+        self.write = display
+        code.InteractiveInterpreter.__init__(self, locals=locals)
+
+    def execute(self, input):
+        old_stdout = sys.stdout
+        sys.stdout = Output(self.write, old_stdout)
+        more = self.runsource(input)
+        sys.stdout = old_stdout
+        return more
+
 
 class InterpreterElement(pyglet.text.document.InlineElement):
-    def __init__(self, content, width=400, height=200, sysver=False):
-        self.width = 600
-        self.height = 300
-        self.content = 'Inner content\nover\nmultiple\nlines'
+    prompt = ">>> "
+    prompt_more = "... "
+
+    def __init__(self, content, width=800, height=300, sysver=False):
+        self.width = width
+        self.height = height
+
+        # set up the interpreter
+        if sysver:
+            self.content = sys.version + '\n' + self.prompt
+        else:
+            self.content = self.prompt
+        self.interpreter = MyInterpreter({}, self._write)
+        self.current_input = []
+        self.history = ['']
+        self.history_pos = 0
+
         super(InterpreterElement, self).__init__(self.height, 0, self.width)
 
     def on_enter(self, w, h):
@@ -86,6 +115,10 @@ class InterpreterElement(pyglet.text.document.InlineElement):
             'font_size': 20, 
             'color': (0, 0, 0, 255),
         })
+        self.start_of_line = len(self.document.text)
+
+    def on_leave(self):
+        self.document = None
 
     def place(self, layout, x, y):
         self.quad = layout.batch.add(4, GL_QUADS, layout.top_group,
@@ -95,15 +128,120 @@ class InterpreterElement(pyglet.text.document.InlineElement):
         self.layout = ScrolledIncrementalTextLayout(self.document,
             self.width, self.height, multiline=True, batch=layout.batch,
             group=layout.top_group)
-        self.layout.top_group.scissor_offset_x = -x
-        self.layout.top_group.scissor_offset_y = -y
+
+        # position
         self.layout.begin_update()
         self.layout.x = x
         self.layout.y = y
         self.layout.valign = 'bottom'
         self.layout.end_update()
 
+        # store off a reference to the surrounding layout's group so we can
+        # modify our scissor based on its screen position (ugh)
+        self.layout.top_group.parent_group = layout.top_group
+
+        self.caret = pyglet.text.caret.Caret(self.layout, color=(0, 0, 0))
+        self.caret.on_activate()
+        self.caret.position = len(self.document.text)
+
     def remove(self, layout):
         self.layout.delete()
         self.quad.delete()
+        self.caret = None
+
+    doing_more = False
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.TAB:
+            return self.caret.on_text('\t')
+        elif symbol in (pyglet.window.key.ENTER, pyglet.window.key.NUM_ENTER):
+            # write the newline
+            self._write('\n')
+
+            line = self.document.text[self.start_of_line:]
+            if line == 'help()':
+                line = 'print "help() not supported, sorry!"'
+            self.current_input.append(line)
+            self.history_pos = len(self.history)
+            if line.strip():
+                self.history[self.history_pos-1] = line.strip()
+                self.history.append('')
+            more = self.interpreter.execute('\n'.join(self.current_input))
+            if self.doing_more and not line.strip():
+                self.doing_more = False
+            more = more or self.doing_more
+            if not more:
+                self.current_input = []
+                self._write(self.prompt)
+            else:
+                self.doing_more = True
+                self._write(self.prompt_more)
+            self.start_of_line = len(self.document.text)
+            self.caret.position = len(self.document.text)
+        elif symbol == pyglet.window.key.SPACE:
+            pass
+        else:
+            return pyglet.event.EVENT_UNHANDLED
+        return pyglet.event.EVENT_HANDLED
+
+    def on_text(self, symbol):
+        # squash carriage return - we already handle them above
+        if symbol == '\r':
+            return pyglet.event.EVENT_HANDLED
+
+        self._scroll_to_bottom()
+        return self.caret.on_text(symbol)
+
+    def on_text_motion(self, motion):
+        at_sol = self.caret.position == self.start_of_line
+
+        if motion == pyglet.window.key.MOTION_UP:
+            # move backward in history, storing the current line of input
+            # if we're at the very end of time
+            line = self.document.text[self.start_of_line:]
+            if self.history_pos == len(self.history)-1:
+                self.history[self.history_pos] = line
+            self.history_pos = max(0, self.history_pos-1)
+            self.document.delete_text(self.start_of_line,
+                len(self.document.text))
+            self._write(self.history[self.history_pos])
+            self.caret.position = len(self.document.text)
+        elif motion == pyglet.window.key.MOTION_DOWN:
+            # move forward in the history
+            self.history_pos = min(len(self.history)-1, self.history_pos+1)
+            self.document.delete_text(self.start_of_line,
+                len(self.document.text))
+            self._write(self.history[self.history_pos])
+            self.caret.position = len(self.document.text)
+        elif motion == pyglet.window.key.MOTION_BACKSPACE:
+            # can't delete the prompt
+            if not at_sol:
+                return self.caret.on_text_motion(motion)
+        elif motion == pyglet.window.key.MOTION_LEFT:
+            # can't move back beyond start of line
+            if not at_sol:
+                return self.caret.on_text_motion(motion)
+        elif motion == pyglet.window.key.MOTION_PREVIOUS_WORD:
+            # can't move back word beyond start of line
+            if not at_sol:
+                return self.caret.on_text_motion(motion)
+        else:
+            return self.caret.on_text_motion(motion)
+        return pyglet.event.EVENT_HANDLED
+
+    def _write(self, s):
+        self.document.insert_text(len(self.document.text), s, {
+            'font_name': 'Courier New',
+            'font_size': 20,
+            'color': (0, 0, 0, 255),
+        })
+        self._scroll_to_bottom()
+
+    def _scroll_to_bottom(self):
+        # on key press always move the view to the bottom of the screen
+        if self.layout.height < self.layout.content_height:
+            self.layout.valign = 'bottom'
+            self.layout.y = 0
+            self.layout.view_y = 0
+        if self.caret.position < self.start_of_line:
+            self.caret.position = len(self.document.text)
 
