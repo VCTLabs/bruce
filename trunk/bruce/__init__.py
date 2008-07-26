a9137bd25fc792f796e668d6e080aeec30ca4534
r1chardj0n3s <r1chardj0n3s@1a22b027-3248-0410-905a-5b7c5a519ae3>
2008-07-26 01:51:43 +0000

    Make Bruce compatible with Python2.6 Fix up missing pygments handling

--- trunk/bruce/__init__.py
+++ trunk/bruce/__init__.py
@@ -1,126 +1,42 @@
+'''Bruce, the Presentation Tool
+
+Bruce is typically invoked using the command-line::
+
+    python -m bruce.run <presentation file>
+
+See HOWTO.txt in the source or the docs online at
+
+    http://r1chardj0n3s.googlepages.com/bruce
+
+for more information.
+
+
+License
+=======
+
+Copyright (c) 2005-2008 Richard Jones <richard@mechanicalcat.net>
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN
+NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''
 __version__ = '3.0beta2'
-
-import os
-import sys
-import time
-from cgi import escape as html_quote
-
-import pyglet
-from cocos.director import director
-
-from bruce import rst_parser
-from bruce import presentation
-from bruce import display_source
-
-def main():
-    from optparse import OptionParser
-    p = OptionParser()
-    p.add_option("-f", "--fullscreen", dest="fullscreen",
-                      action="store_true", default=False,
-                      help="run in fullscreen mode")
-    p.add_option("-t", "--timer", dest="timer",
-                      action="store_true", default=False,
-                      help="display a timer")
-    p.add_option("-p", "--pagecount", dest="page_count",
-                      action="store_true", default=False,
-                      help="display page numbers")
-    p.add_option("-s", "--startpage", dest="start_page",
-                      default="1",
-                      help="start at page N (1+, default 1)")
-    p.add_option("-S", "--screen", dest="screen",
-                      default="1",
-                      help="display on screen (1+, default 1)")
-    #p.add_option("-n", "--notes", dest="notes",
-    #                  action="store_true", default=False,
-    #                  help="generate HTML notes (do not run presentation)")
-    #p.add_option("-o", "--out-file", dest="out_file",
-    #                  default="",
-    #                  help="filename to write notes to (default stdout)")
-    #p.add_option("-c", "--columns", dest="columns",
-    #                  default="2",
-    #                  help="number of columns in notes (default 2)")
-    p.add_option("-v", "--version", dest="version",
-                      action="store_true", default=False,
-                      help="display version and quit")
-    #p.add_option("-d", "--progress-screen", dest="progress_screen",
-    #                  default=None,
-    #                  help="display progress in screen (1+, default none)")
-    p.add_option("-D", "--display-source", dest="source",
-                      action="store_true", default=False,
-                      help="display source in terminal")
-    p.add_option("-w", "--window-size", dest="window_size",
-                      default="1024x768",
-                      help="size of the window when not fullscreen")
-
-    (options, args) = p.parse_args()
-
-    if options.version:
-        print __version__
-        sys.exit(0)
-
-    elif not args:
-        print 'Error: argument required'
-        print p.get_usage()
-        sys.exit(1)
-
-    filename = args[0]
-
-    #elif options.notes:
-    #    notes(args[0], options.out_file, int(options.columns))
-
-    display = pyglet.window.get_platform().get_default_display()
-    screen = int(options.screen)-1
-    screen = display.get_screens()[screen]
-    #progress_screen = None
-    #if options.progress_screen:
-    #    progress_screen = int(options.progress_screen)-1
-    #    progress_screen = display.get_screens()[progress_screen]
-    width, height = map(int, options.window_size.split('x'))
-    width = min(width, screen.width)
-    height = min(height, screen.height)
-    screen=int(options.screen)-1
-
-    directory = os.path.abspath(os.path.dirname(filename))
-    pyglet.resource.path.append(directory)
-    pyglet.resource.reindex()
-
-    display = pyglet.window.get_platform().get_default_display()
-    screen = display.get_screens()[screen]
-    if options.fullscreen:
-        director.init(fullscreen=options.fullscreen,
-            screen=screen, do_not_scale=True)
-    else:
-        director.init(width=width, height=height,
-            screen=screen, do_not_scale=True)
-
-    content = file(filename).read()
-    pres = presentation.Presentation(rst_parser.parse(content),
-        show_timer=options.timer, show_count=options.page_count,
-        start_page=int(options.start_page)-1,
-        desired_size=(width, height))
-
-    director.window.push_handlers(pres)
-
-#    if progress_screen is not None:
-#        pw = min(1280, progress_screen.width)
-#        ph = min(480, progress_screen.height)
-#        if options.fullscreen:
-#            pw = pyglet.window.Window(fullscreen=options.fullscreen,
-#                screen=progress_screen)
-#        else:
-#            pw = pyglet.window.Window(pw, ph, screen=progress_screen)
-#        prog = progress.Progress(pw, pres, content.decode('utf8'))
-#        pw.push_handlers(prog)
-#        pres.push_handlers(prog)
-#        prog.push_handlers(pres)
-
-    if options.source:
-        pres.push_handlers(display_source.DisplaySource())
-
-    # now that we're all set up, load up the first page
-    pres.start_presentation()
-
-
-if __name__ == '__main__':
-    main()
-
