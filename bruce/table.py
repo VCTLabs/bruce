a426226448d369d32569cc88a4ec244523716b37
r1chardj0n3s <r1chardj0n3s@1a22b027-3248-0410-905a-5b7c5a519ae3>
2008-12-23 04:05:01 +0000

    Handle fitting table width to cells. Add cell padding. Add border rendering. Add handling of headings.

--- bruce/table.py
+++ bruce/table.py
@@ -20,14 +20,13 @@ class TableElement(pyglet.text.document.InlineElement):
         self.stylesheet = stylesheet
 
         # rough numbers to start with
-        self.width = director.get_window_size()[0]
 
         self.table = TableGenerator(self)
         doctree_node.walkabout(TableVisitor(doctree_node, self.table))
         self.dpi = 96
-        self.table.layout(self.width)
+        self.table.layout()
 
-        super(TableElement, self).__init__(self.table.height, 0, self.width)
+        super(TableElement, self).__init__(self.table.height, 0, self.table.width)
 
     def set_scale(self, scale):
         # XXX relayout table
@@ -61,62 +60,99 @@ class TableVisitor(rst_parser.DocutilsVisitor):
 class TableGenerator(object):
     def __init__(self, element):
         self.element = element
-        self.rows = self.columns = 0
+        self.num_rows = self.num_columns = 0
         self.cells = {}
         self.cell_layouts = {}
         self.column_specs = []
         self.cell_decoration = {}
+        self.body_rows = set()
+        self.heading_rows = set()
 
         # layout spec
         # XXX maybe allow multiple layouts?
         self.parent_layout = self.x = self.y = None
 
-    def layout(self, width):
+    def layout(self):
+        width = director.get_window_size()[0]
+
         # figure column widths
         specwidth = float(sum(self.column_specs))
-        self.colwidths = [int(w/specwidth*width)
+        spec_widths = [int(w/specwidth*width)
             for w in self.column_specs]
+
+        # padding
+        style = self.element.stylesheet['table']
+        vpad = style['top_padding'] + style['bottom_padding']
+        hpad = style['left_padding'] + style['right_padding']
 
         # figure row heights
         self.row_heights = []
-        for row in range(self.rows):
+        self.column_widths = [0]*self.num_columns
+        for row in range(self.num_rows):
             rowheight = 0
-            for col in range(self.columns):
-                colwidth = self.colwidths[col]
+            for col in range(self.num_columns):
+                width = spec_widths[col]
                 document = self.cells[row, col]
                 l = pyglet.text.layout.IncrementalTextLayout(
-                    document, colwidth, 50, dpi=self.element.dpi,
+                    document, width, 50, dpi=self.element.dpi,
                     multiline=True)
                 rowheight = max(rowheight, l.content_height)
+                width = l.content_width + hpad
+                width += 1      # seems to be a rounding error
+                self.column_widths[col] = max(width, self.column_widths[col])
                 l.delete()
-                # XXX figure columnd widths too
-            self.row_heights.append(rowheight)
+            if style['border']:
+                if row in self.heading_rows:
+                    # heading row
+                    rowheight += 3
+                elif row != self.num_rows - 1:
+                    # regular row
+                    rowheight += 1
+            self.row_heights.append(rowheight+vpad)
         self.height = sum(self.row_heights)
+        self.width = sum(self.column_widths)
 
     def full_layout(self, layout):
+        self.parent_layout = layout
+        self.x = self.y = None
+
         # organise the layers
         foreground = pyglet.graphics.OrderedGroup(1, layout.top_group)
         background = pyglet.graphics.OrderedGroup(0, layout.top_group)
 
+        # style info
+        style = self.element.stylesheet['table']
+        vpad = style['top_padding'] + style['bottom_padding']
+        hpad = style['left_padding'] + style['right_padding']
+
         # place in the real layout
-        for row in range(self.rows):
-            height = self.row_heights[row]
-            for col in range(self.columns):
-                colwidth = self.colwidths[col]
+        for row in range(self.num_rows):
+            height = self.row_heights[row]-vpad
+            if style['border']:
+                if row in self.heading_rows:
+                    # heading row
+                    height -= 3
+                elif row != self.num_rows - 1:
+                    # regular row
+                    height -= 1
+            for col in range(self.num_columns):
                 document = self.cells[row, col]
                 l = pyglet.text.layout.IncrementalTextLayout(
-                    document, colwidth, height, dpi=self.element.dpi,
-                    multiline=True, batch=layout.batch, group=foreground)
+                    document, self.column_widths[col]-hpad, height,
+                    dpi=self.element.dpi, multiline=True,
+                    batch=layout.batch, group=foreground)
                 self.cell_layouts[row, col] = l
 
-        style = self.element.stylesheet['table']
-        for row in range(self.rows):
+        # cell backgrounds
+        for row in range(self.num_rows):
             height = self.row_heights[row]
             x = 0
-            for col in range(self.columns):
-                colwidth = self.colwidths[col]
+            for col in range(self.num_columns):
+                colwidth = self.column_widths[col]
                 x2 = x + colwidth
-                if row%2:
+                if row in self.heading_rows:
+                    color = style['heading_background_color']
+                elif row%2:
                     color = style['odd_background_color']
                 else:
                     color = style['even_background_color']
@@ -127,8 +163,15 @@ class TableGenerator(object):
                 x = x2
                 self.cell_decoration[row, col] = r
 
-        self.parent_layout = layout
-        self.x = self.y = None
+        # border lines
+        if style['border']:
+            color = style['border_color']
+            l = [0] * 4 * (self.num_rows-1 + self.num_columns-1)
+            n = len(l)//2
+            self.border_decoration = layout.batch.add(n,
+                pyglet.gl.GL_LINES, foreground,
+                ('v2i', l), ('c4B', color * n),
+            )
 
     _r = None
     def place(self, layout, x, y):
@@ -145,29 +188,46 @@ class TableGenerator(object):
         self.move_layout(x, y)
 
     def move_layout(self, x, y):
+        # style info
+        style = self.element.stylesheet['table']
+
         # we lay out from the top so start at the top
         ry = y + sum(self.row_heights)
-        for row in range(self.rows):
+        for row in range(self.num_rows):
             cx = x
             height = self.row_heights[row]
-            for column in range(self.columns):
+            for column in range(self.num_columns):
                 # update layout
                 l = self.cell_layouts[row, column]
                 l.begin_update()
                 l.anchor_y = 'top'
-                l.y = ry
-                l.x = cx
+                l.y = ry - style['top_padding']
+                l.x = cx + style['left_padding']
                 l.end_update()
 
                 # update decoration
-                colwidth = self.colwidths[column]
+                colwidth = self.column_widths[column]
                 x2 = cx + colwidth
                 r = self.cell_decoration[row, column]
                 r.vertices[:] = [cx, ry-height, x2, ry-height, x2, ry, cx, ry]
 
-                cx += self.colwidths[column]
+                cx += self.column_widths[column]
 
             ry -= height
+
+        if style['border']:
+            w = sum(self.column_widths)
+            h = sum(self.row_heights)
+            l = []
+            ry = y + sum(self.row_heights)
+            for row in range(self.num_rows-1):
+                ry -= self.row_heights[row]
+                l.extend([x, ry, x+w, ry])
+            cx = x
+            for col in range(self.num_columns-1):
+                cx += self.column_widths[col]
+                l.extend([cx, y, cx, y+h])
+            self.border_decoration.vertices[:] = l
 
         self.x, self.y = x, y
 
@@ -178,6 +238,8 @@ class TableGenerator(object):
         for v in self.cell_decoration.values():
             v.delete()
         self.cell_decoration = {}
+        self.border_decoration.delete()
+        self.border_decoration = None
         self.layout = self.x = self.y = None
 
     def pop_style(self, *args):
@@ -186,16 +248,20 @@ class TableGenerator(object):
 
     def visit_table(self, node): pass
     def visit_tgroup(self, node): pass
-    def visit_tbody(self, node): pass
+    def visit_tbody(self, node):
+        self._content_section = self.body_rows
+    def visit_thead(self, node):
+        self._content_section = self.heading_rows
 
     def visit_colspec(self, node):
         self.column_specs.append(int(node['colwidth']))
 
     def visit_row(self, node):
-        self.columns = 0
+        self.num_columns = 0
+        self._content_section.add(self.num_rows)
 
     def depart_row(self, node):
-        self.rows += 1
+        self.num_rows += 1
 
     def visit_unknown(self, node):
         raise ValueError('UNKNOWN %s'%node.__class__.__name__)
@@ -205,7 +271,7 @@ class TableGenerator(object):
     def visit_entry(self, node):
         # XXX assert no col/row spanning etc
         g = rst_parser.DocumentGenerator(self.element.stylesheet, None)
-        self.cells[self.rows, self.columns] = g.decode(node)
-        self.columns += 1
+        self.cells[self.num_rows, self.num_columns] = g.decode(node)
+        self.num_columns += 1
         raise nodes.SkipNode
 
