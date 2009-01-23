368d557fb1884136cb1fa12a833153c316d57999
r1chardj0n3s <r1chardj0n3s@1a22b027-3248-0410-905a-5b7c5a519ae3>
2009-01-23 00:08:54 +0000

    Add test for smartypants behavior.
    Make -- be en-dash and --- be em-dash how *I* like it ;)

---
+++ trunk/examples/test_smartypants.py
@@ -0,0 +1,22 @@
+"Testing curly-double-quotes"
+
+'Testing curly-double-quotes'
+
+'Twas started with the incorrect quote
+
+But isn't this one correct?
+
+And some ... elipsis, en -- dash and em --- dash.
+
+Make sure::
+
+   "this pre section is not transformed"
+
+   -- double dash
+
+   --- triple dash
+
+   ... dot dot dot
+
+Unfortunately \"escaped quotes\" don't work.
+
